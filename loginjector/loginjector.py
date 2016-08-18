cac5fe5b863c46468d10ebc729b41ce65ec24be1
dave <dave_gitlab@davepedu.com>
2016-08-17 19:25:40 -0700

    add some things

---
+++ loginjector/loginjector.py
@@ -0,0 +1,11 @@
+
+import subprocess
+import logging
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+from loginjector import __version__
+
+setup(name='loginjector',
+      version=__version__,
+      description='loginjector daemon',
+      url='http://gitlab.xmopx.net/dave/loginjector',
+      author='dpedu',
+      author_email='dave@davepedu.com',
+      packages=['loginjector'],
+      entry_points={
+          'console_scripts': [
+              'loginjector_daemon = loginjector.loginjector:cli',
+          ]
+      },
+      #install_requires=['requests==2.10.0']
+      )
